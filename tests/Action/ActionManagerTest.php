920299bbf5ca45a112e25794a68458711b269ad2
Korvin Szanto <korvinszanto@gmail.com>
2015-07-01 11:04:55 -0700

    Add more test coverage

--- tests/Action/ActionManagerTest.php
+++ tests/Action/ActionManagerTest.php
@@ -17,4 +17,22 @@ class ActionManagerTest extends \PHPUnit_Framework_TestCase
 
         $manager->handleConnect(new Connection($manager, ''));
     }
+
+    public function testHandleTick()
+    {
+
+        $ping_mock = $this->getMock('\Buttress\IRC\Action\ActionInterface');
+
+        $mock = $this->getMock('\Buttress\IRC\Action\ActionInterface');
+        $mock->expects($this->once())
+            ->method('handleTick')
+            ->with($this->isInstanceOf('\Buttress\IRC\Connection\ConnectionInterface'));
+
+        $factory = new MessageFactory();
+        $manager = new ActionManager($factory, $ping_mock);
+        $manager->add('tick', $mock);
+
+        $manager->handleTick(new Connection($manager, ''));
+    }
+
 }
